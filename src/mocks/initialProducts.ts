1d4973eeddda778d803e8056b4050ec687a9a78f
Josue Garces Jouvin <josuejouvin27@gmail.com>
2025-12-30 20:49:14 -0500

    Add initial product mock data

---
+++ src/mocks/initialProducts.ts
@@ -0,0 +1,82 @@
+const initialProducts = [
+  {
+    id: 1,
+    name: 'PlayStation 5',
+    price: 499.99,
+    image:
+      'https://images.unsplash.com/photo-1606813907291-d86efa9b94db?w=400&h=300&fit=crop',
+    favorite: false,
+  },
+  {
+    id: 2,
+    name: 'PC Gamer RTX 4090',
+    price: 2499.99,
+    image:
+      'https://images.unsplash.com/photo-1587202372634-32705e3bf49c?w=400&h=300&fit=crop',
+    favorite: false,
+  },
+  {
+    id: 3,
+    name: 'Nintendo Switch OLED',
+    price: 349.99,
+    image:
+      'https://images.unsplash.com/photo-1578303512597-81e6cc155b3e?w=400&h=300&fit=crop',
+    favorite: false,
+  },
+  {
+    id: 4,
+    name: 'Xbox Series X',
+    price: 499.99,
+    image:
+      'https://images.unsplash.com/photo-1621259182978-fbf93132d53d?w=400&h=300&fit=crop',
+    favorite: false,
+  },
+  {
+    id: 5,
+    name: 'MacBook Pro M3',
+    price: 1999.99,
+    image:
+      'https://images.unsplash.com/photo-1517336714731-489689fd1ca8?w=400&h=300&fit=crop',
+    favorite: false,
+  },
+  {
+    id: 6,
+    name: 'Monitor Gaming 4K 144Hz',
+    price: 799.99,
+    image:
+      'https://images.unsplash.com/photo-1527443224154-c4a3942d3acf?w=400&h=300&fit=crop',
+    favorite: false,
+  },
+  {
+    id: 7,
+    name: 'Teclado Mecánico RGB',
+    price: 149.99,
+    image:
+      'https://images.unsplash.com/photo-1511467687858-23d96c32e4ae?w=400&h=300&fit=crop',
+    favorite: false,
+  },
+  {
+    id: 8,
+    name: 'Mouse Gamer Inalámbrico',
+    price: 89.99,
+    image:
+      'https://images.unsplash.com/photo-1527864550417-7fd91fc51a46?w=400&h=300&fit=crop',
+    favorite: false,
+  },
+  {
+    id: 9,
+    name: 'Audífonos Gaming 7.1',
+    price: 179.99,
+    image:
+      'https://images.unsplash.com/photo-1546435770-a3e426bf472b?w=400&h=300&fit=crop',
+    favorite: false,
+  },
+  {
+    id: 10,
+    name: 'Silla Gamer Ergonómica',
+    price: 399.99,
+    image:
+      'https://images.unsplash.com/photo-1598550476439-6847785fcea6?w=400&h=300&fit=crop',
+    favorite: false,
+  },
+];
